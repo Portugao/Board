b0cc935a0e9c80ec11b91947c36f3dc2e558a824
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-06-20 19:14:52 +0200

    rule Abo and count of view

--- src/modules/MUBoard/lib/MUBoard/Util/Model.php
+++ src/modules/MUBoard/lib/MUBoard/Util/Model.php
@@ -16,15 +16,38 @@
  */
 class MUBoard_Util_Model extends MUBoard_Util_Base_Model
 {
-	
+
 	/**
 	 * This function counts the call of a posting
 	 * @param id   id of the ticket to close
 	 */
 
-	public static function closeTicket($id) {
+	public static function closePosting($id) {
 
-		// build ticket repository
+		// build posting repository
+		$repository = MUBoard_Util_Model::getPostingRepository();
+
+		$entity = $repository->selectById($id);
+
+		$serviceManager = ServiceUtil::getManager();
+		$entityManager = $serviceManager->getService('doctrine.entitymanager');
+
+		$entity->setState(0);
+
+		$entityManager->flush();
+
+		return true;
+
+	}
+
+	/**
+	 * This function counts the call of a posting
+	 * @param id   id of the ticket to close
+	 */
+
+	public static function addView($id) {
+
+		// build posting repository
 		$repository = MUBoard_Util_Model::getPostingRepository();
 
 		$entity = $repository->selectById($id);
@@ -40,7 +63,113 @@ class MUBoard_Util_Model extends MUBoard_Util_Base_Model
 		return true;
 
 	}
-	
+
+	/**
+	 * This function counts the call of a posting
+	 * @param posting   id of the posting to take abo
+	 * @param forum     id of the forum to take abo
+	 * @param category  id of the category to take abo
+	 */
+
+	public static function takeAbo($posting = 0, $forum = 0, $category = 0) {
+
+		// Security ckeck
+		if (SecurityUtil::checkPermission('MUBoard::', '::', ACCESS_COMMENT)) {
+
+			// get actual user id
+			$userid = UserUtil::getVar('uid');
+			// get new Abo object
+			$entity = new MUBoard_Entity_Abo();
+			// take abo of posting if posting is taken
+			if ($posting > 0) {
+				$entity->setPostingid($posting);
+			}
+			// take abo of forum if forum is taken
+			if ($forum > 0) {
+				$entity->setForumid($forum);
+			}
+			// take abo of category if category is taken
+			if ($category > 0) {
+				$entity->setCatid($category);
+			}
+
+			$entity->setUserid($userid);
+
+			$serviceManager = ServiceUtil::getManager();
+			$entityManager = $serviceManager->getService('doctrine.entitymanager');
+
+			$entityManager->persist($entity);
+			$entityManager->flush();
+
+
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+
+	/**
+	 * This function counts the call of a posting
+	 * @param posting   id of the posting to quit abo
+	 * @param forum     id of the forum to quit abo
+	 * @param category  id of the category to quit abo
+	 */
+
+	public static function quitAbo($posting = 0, $forum = 0, $category = 0) {
+
+		// build posting repository
+		$repository = MUBoard_Util_Model::getAboRepository();
+		// get actual user id
+		$userid = UserUtil::getVar('uid');
+		// quit abo of posting if posting is taken
+		if ($posting > 0) {
+			// look for abo
+			$where = 'tbl.postingid = \'' . DataUtil::formatForStore($posting) . '\'';
+			$where .= ' AND ';
+			$where .= 'tbl.userid = \'' . DataUtil::formatForStore($userid) . '\'';
+			$abos = $repository->selectWhere($where);
+			$serviceManager = ServiceUtil::getManager();
+			$entityManager = $serviceManager->getService('doctrine.entitymanager');
+			foreach ($abos as $abo) {
+				$entityManager->remove($abo);
+				$entityManager->flush();
+			}
+		}
+
+		if ($forum > 0) {
+			// look for abo
+			$where = 'tbl.forumid = \'' . DataUtil::formatForStore($forum) . '\'';
+			$where .= ' AND ';
+			$where .= 'tbl.userid = \'' . DataUtil::formatForStore($userid) . '\'';
+			$abos = $repository->selectWhere($where);
+			$serviceManager = ServiceUtil::getManager();
+			$entityManager = $serviceManager->getService('doctrine.entitymanager');
+			foreach ($abos as $abo) {
+				$entityManager->remove($abo);
+				$entityManager->flush();
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 *
+	 This method is for getting a repository for abo
+	 *
+	 */
+
+	public static function getAboRepository() {
+
+		$serviceManager = ServiceUtil::getManager();
+		$entityManager = $serviceManager->getService('doctrine.entitymanager');
+		$repository = $entityManager->getRepository('MUBoard_Entity_Abo');
+
+		return $repository;
+	}
+
 	/**
 	 *
 	 This method is for getting a repository for category
@@ -55,7 +184,7 @@ class MUBoard_Util_Model extends MUBoard_Util_Base_Model
 
 		return $repository;
 	}
-	
+
 	/**
 	 *
 	 This method is for getting a repository for forum
@@ -70,7 +199,7 @@ class MUBoard_Util_Model extends MUBoard_Util_Base_Model
 
 		return $repository;
 	}
-	
+
 	/**
 	 *
 	 This method is for getting a repository for posting
