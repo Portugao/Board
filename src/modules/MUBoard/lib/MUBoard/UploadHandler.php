75de9e0a2bb4a0fca4fae9f1b597c65163498da3
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2013-05-25 14:36:03 +0200

    fixed #15

--- src/modules/MUBoard/lib/MUBoard/UploadHandler.php
+++ src/modules/MUBoard/lib/MUBoard/UploadHandler.php
@@ -16,24 +16,24 @@
  */
 class MUBoard_UploadHandler extends MUBoard_Base_UploadHandler
 {
-	/**
-	 * var array List of allowed file sizes per field.
-	 */
-	protected $allowedFileSizes;
-
-	/**
-	 * Constructor initialising the supported object types.
-	 */
-	public function __construct()
-	{
-		$this->allowedObjectTypes = array('posting');
-		$this->imageFileTypes = array('gif', 'jpeg', 'jpg', 'png');
-		$this->forbiddenFileTypes = array('cgi', 'pl', 'asp', 'phtml', 'php', 'php3', 'php4', 'php5', 'exe', 'com', 'bat', 'jsp', 'cfm', 'shtml');
-		$filesize = ModUtil::getVar('MUBoard', 'allowedSizeOfImages');
-		$this->allowedFileSizes = array('posting' => array('firstImage' => $filesize, 'secondImage' => $filesize, 'thirdImage' => $filesize));
-
-	}
-	
+    /**
+     * var array List of allowed file sizes per field.
+     */
+    protected $allowedFileSizes;
+    
+    /**
+     * Constructor initialising the supported object types.
+     */
+    public function __construct()
+    {
+        $this->allowedObjectTypes = array('posting', 'rank');
+        $this->imageFileTypes = array('gif', 'jpeg', 'jpg', 'png');
+        $this->forbiddenFileTypes = array('cgi', 'pl', 'asp', 'phtml', 'php', 'php3', 'php4', 'php5', 'exe', 'com', 'bat', 'jsp', 'cfm', 'shtml');
+        $filesize = ModUtil::getVar('MUBoard', 'allowedSizeOfImages');
+        $this->allowedFileSizes = array('posting' => array('firstImage' => $filesize, 'secondImage' => $filesize, 'thirdImage' => $filesize));
+    
+    }
+    
     /**
      * Process a file upload.
      *
@@ -46,21 +46,21 @@ class MUBoard_UploadHandler extends MUBoard_Base_UploadHandler
     public function performFileUpload($objectType, $fileData, $fieldName)
     {
         $dom = ZLanguage::getModuleDomain('MUBoard');
-
+    
         $result = array('fileName' => '',
-            'metaData' => array());
-
+                'metaData' => array());
+    
         // check whether uploads are allowed for the given object type
         if (!in_array($objectType, $this->allowedObjectTypes)) {
             return $result;
         }
-
+    
         // perform validation
         if (!$this->validateFileUpload($objectType, $fileData[$fieldName], $fieldName)) {
             // skip this upload field
             return false; //$result;
         }
-
+    
         // retrieve the final file name
         $fileName = $fileData[$fieldName]['name'];
         $fileNameParts = explode('.', $fileName);
@@ -68,90 +68,90 @@ class MUBoard_UploadHandler extends MUBoard_Base_UploadHandler
         $extension = str_replace('jpeg', 'jpg', strtolower($extension));
         $fileNameParts[count($fileNameParts) - 1] = $extension;
         $fileName = implode('.', $fileNameParts);
-
+    
         // retrieve the final file name
-        $basePath = MUImage_Util_Controller::getFileBaseFolder($objectType, $fieldName);
+        $basePath = MUBoard_Util_Controller::getFileBaseFolder($objectType, $fieldName);
         $fileName = $this->determineFileName($objectType, $fieldName, $basePath, $fileName, $extension);
-
+    
         if (!move_uploaded_file($fileData[$fieldName]['tmp_name'], $basePath . $fileName)) {
             return LogUtil::registerError(__('Error! Could not move your file to the destination folder.', $dom));
         }
-
+    
         // collect data to return
         $result['fileName'] = $fileName;
         $result['metaData'] = $this->readMetaDataForFile($fileName, $basePath . $fileName);
-
+    
         return $result;
     }
-
-
-	/**
-	 * Check if an upload file meets all validation criteria.
-	 *
-	 * @param array $file Reference to data of uploaded file.
-	 *
-	 * @return boolean true if file is valid else false
-	 */
-	protected function validateFileUpload($objectType, $file, $fieldName)
-	{
-		$dom = ZLanguage::getModuleDomain('MUBoard');
-
-		// check if a file has been uploaded properly without errors
-		if ((!is_array($file)) || (is_array($file) && ($file['error'] != '0'))) {
-			if (is_array($file)) {
-				return $this->handleError($file);
-			}
-			return LogUtil::registerError(__('Error! No file found.', $dom));
-		}
-
-		$maxSize = $this->allowedFileSizes[$objectType][$fieldName];
-
-		if ($maxSize > 0) {
-
-			$fileSize = filesize($file['tmp_name']);
-
-			if ($fileSize > $maxSize) {
-				$maxSizeKB = $maxSize / 1024;
-
-				if ($maxSizeKB < 1024) {
-					$maxSizeKB = DataUtil::formatNumber($maxSizeKB);
-					$fileName = $file['name'];
+    
+    
+    /**
+     * Check if an upload file meets all validation criteria.
+     *
+     * @param array $file Reference to data of uploaded file.
+     *
+     * @return boolean true if file is valid else false
+     */
+    protected function validateFileUpload($objectType, $file, $fieldName)
+    {
+        $dom = ZLanguage::getModuleDomain('MUBoard');
+    
+        // check if a file has been uploaded properly without errors
+        if ((!is_array($file)) || (is_array($file) && ($file['error'] != '0'))) {
+            if (is_array($file)) {
+                return $this->handleError($file);
+            }
+            return LogUtil::registerError(__('Error! No file found.', $dom));
+        }
+    
+        $maxSize = $this->allowedFileSizes[$objectType][$fieldName];
+    
+        if ($maxSize > 0) {
+    
+            $fileSize = filesize($file['tmp_name']);
+    
+            if ($fileSize > $maxSize) {
+                $maxSizeKB = $maxSize / 1024;
+    
+                if ($maxSizeKB < 1024) {
+                    $maxSizeKB = DataUtil::formatNumber($maxSizeKB);
+                    $fileName = $file['name'];
                     LogUtil::registerError(__f('Error! Your file %s is too big. Please keep it smaller than %s kilobytes.', array($fileName, $maxSizeKB), $dom));
-					return false;
-
-				}
-
-				$maxSizeMB = $maxSizeKB / 1024;
-				$maxSizeMB = DataUtil::formatNumber($maxSizeMB);
-				$fileName = $file['name'];
+                    return false;
+    
+                }
+    
+                $maxSizeMB = $maxSizeKB / 1024;
+                $maxSizeMB = DataUtil::formatNumber($maxSizeMB);
+                $fileName = $file['name'];
                 LogUtil::registerError(__f('Error! Your file is too big. Please keep it smaller than %s megabytes.', array($fileName, $maxSizeKB), $dom));
-				return false; 
-			}
-
-		}
-
-		// extract file extension
-		$fileName = $file['name'];
-		$extensionarr = explode('.', $fileName);
-		$extension = strtolower($extensionarr[count($extensionarr) - 1]);
-
-		// validate extension
-		$isValidExtension = $this->isAllowedFileExtension($objectType, $fieldName, $extension);
-		if ($isValidExtension === false) {
-			LogUtil::registerError(__('Error! This file type is not allowed. Please choose another file format.', $dom));
-			
-		}
-
-		// validate image file
-		$imgInfo = array();
-		$isImage = in_array($extension, $this->imageFileTypes);
-		if ($isImage) {
-			$imgInfo = getimagesize($file['tmp_name']);
-			if (!is_array($imgInfo) || !$imgInfo[0] || !$imgInfo[1]) {
-				return LogUtil::registerError(__('Error! This file type seems not to be a valid image.', $dom));
-			}
-		}
-
-		return true;
-	}
+                return false;
+            }
+    
+        }
+    
+        // extract file extension
+        $fileName = $file['name'];
+        $extensionarr = explode('.', $fileName);
+        $extension = strtolower($extensionarr[count($extensionarr) - 1]);
+    
+        // validate extension
+        $isValidExtension = $this->isAllowedFileExtension($objectType, $fieldName, $extension);
+        if ($isValidExtension === false) {
+            LogUtil::registerError(__('Error! This file type is not allowed. Please choose another file format.', $dom));
+            	
+        }
+    
+        // validate image file
+        $imgInfo = array();
+        $isImage = in_array($extension, $this->imageFileTypes);
+        if ($isImage) {
+            $imgInfo = getimagesize($file['tmp_name']);
+            if (!is_array($imgInfo) || !$imgInfo[0] || !$imgInfo[1]) {
+                return LogUtil::registerError(__('Error! This file type seems not to be a valid image.', $dom));
+            }
+        }
+    
+        return true;
+    }
 }
