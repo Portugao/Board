2598e7948c80c2fdae3df06f6de15974645d85a1
Michael Ueberschaer <info@homepages-mit-zikula.de>
2018-08-08 10:20:40 +0200

    upgrade

--- MU/BoardModule/BoardModuleInstaller.php
+++ MU/BoardModule/BoardModuleInstaller.php
@@ -19,5 +19,40 @@ use MU\BoardModule\Base\AbstractBoardModuleInstaller;
  */
 class BoardModuleInstaller extends AbstractBoardModuleInstaller
 {
-    // feel free to extend the installer here
+    /**
+     * Upgrade the MUBoardModule application from an older version.
+     *
+     * If the upgrade fails at some point, it returns the last upgraded version.
+     *
+     * @param integer $oldVersion Version to upgrade from
+     *
+     * @return boolean True on success, false otherwise
+     *
+     * @throws RuntimeException Thrown if database tables can not be updated
+     */
+    public function upgrade($oldVersion)
+    {
+        /*
+         $logger = $this->container->get('logger');
+         
+         // Upgrade dependent on old version number
+         switch ($oldVersion) {
+         case '1.0.0':
+         // do something
+         // ...
+         // update the database schema
+         try {
+         $this->schemaTool->update($this->listEntityClasses());
+         } catch (\Exception $exception) {
+         $this->addFlash('error', $this->__('Doctrine Exception') . ': ' . $exception->getMessage());
+         $logger->error('{app}: Could not update the database tables during the upgrade. Error details: {errorMessage}.', ['app' => 'MUBoardModule', 'errorMessage' => $exception->getMessage()]);
+         
+         return false;
+         }
+         }
+         */
+        
+        // update successful
+        return true;
+    }
 }
